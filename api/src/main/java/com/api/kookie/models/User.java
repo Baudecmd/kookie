a7c201ba85fbb6c06e01c6f901e4810fd68945f9
Jonelle Amio <jonelle.amio@gmail.com>
2021-03-25 18:12:17 +0000

    feat(api) : authentification

---
+++ api/src/main/java/com/api/kookie/models/User.java
@@ -0,0 +1,70 @@
+package com.api.kookie.models;
+
+
+import javax.annotation.security.DeclareRoles;
+import javax.persistence.*;
+
+@Entity
+@DeclareRoles("USER")
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY) long id;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public User() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}

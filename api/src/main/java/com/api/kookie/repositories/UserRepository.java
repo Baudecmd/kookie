1d4f26ab27e03df5ae233b4e38aae2225beda0fa
Conrad Baudelet <conradbaudelet@gmail.com>
2021-03-25 18:12:42 +0100

    Feat: Auth and signup

---
+++ api/src/main/java/com/api/kookie/repositories/UserRepository.java
@@ -0,0 +1,21 @@
+package com.api.kookie.repositories;
+
+import com.api.kookie.models.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    @Query(" select u from User u " +
+            " where u.username = ?1"+
+            " and   u.password = ?2")
+    User findByIdAndPassword(String username,String password);
+
+    @Query(" select u from User u " +
+            " where u.username = ?1"+
+            " or   u.email = ?2")
+    List<User> findByUsernameOrEmail(String username, String email);
+}

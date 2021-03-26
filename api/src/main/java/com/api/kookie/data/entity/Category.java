38927a900c23f004f8bbac62bd00e1c05fa0117b
Audran Landuyt <audran.landuyt.etu@univ-lille.fr>
2021-03-26 17:23:30 +0100

    Create Category Entity

---
+++ api/src/main/java/com/api/kookie/data/entity/Category.java
@@ -0,0 +1,55 @@
+package com.api.kookie.data.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @OneToMany(cascade = CascadeType.ALL)
+    Set<Ingredient> ingredients;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}

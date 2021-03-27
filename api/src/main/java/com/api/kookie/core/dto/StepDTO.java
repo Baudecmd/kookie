7807892a77784d82cde6344e654546993168d445
Audran Landuyt <audran.landuyt.etu@univ-lille.fr>
2021-03-27 21:20:07 +0100

    Create Step, StepLine and its DTOs & Parsers

---
+++ api/src/main/java/com/api/kookie/core/dto/StepDTO.java
@@ -0,0 +1,72 @@
+package com.api.kookie.core.dto;
+
+import java.util.Objects;
+
+public class StepDTO {
+
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private int duration;
+
+    public StepDTO() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StepDTO)) return false;
+        StepDTO stepDTO = (StepDTO) o;
+        return getDuration() == stepDTO.getDuration() && Objects.equals(getId(), stepDTO.getId()) && Objects.equals(getName(), stepDTO.getName()) && Objects.equals(getDescription(), stepDTO.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDescription(), getDuration());
+    }
+
+    @Override
+    public String toString() {
+        return "StepDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
---
+++ api/src/main/java/com/api/kookie/core/util/StepLineParser.java
@@ -0,0 +1,46 @@
+package com.api.kookie.core.util;
+
+import com.api.kookie.core.dto.StepLineDTO;
+import com.api.kookie.data.entity.StepLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StepLineParser {
+
+    public static StepLineDTO toDTO(StepLine stepLine) {
+        StepLineDTO stepLineDTO = new StepLineDTO();
+        if (stepLine != null) {
+            stepLineDTO.setId(stepLine.getId());
+            stepLineDTO.setRecetteDTO(RecetteParser.toDTO(stepLine.getRecette()));
+            stepLineDTO.setStepDTO(StepParser.toDTO(stepLine.getStep()));
+        }
+        return stepLineDTO;
+    }
+
+    public static StepLine toEntity(StepLineDTO stepLineDTO) {
+        StepLine stepLine = new StepLine();
+        if (stepLineDTO != null) {
+            if (stepLineDTO.getId() != null) stepLine.setId(stepLineDTO.getId());
+            stepLine.setRecette(RecetteParser.toEntity(stepLineDTO.getRecetteDTO()));
+            stepLine.setStep(StepParser.toEntity(stepLineDTO.getStepDTO()));
+        }
+        return stepLine;
+    }
+
+    public static List<StepLineDTO> parseListToDTO(List<StepLine> stepLines) {
+        List<StepLineDTO> listStepLinesDTO = new ArrayList<>();
+        for (StepLine stepLine : stepLines) {
+            listStepLinesDTO.add(toDTO(stepLine));
+        }
+        return listStepLinesDTO;
+    }
+
+    public static List<StepLine> parseListToEntity(List<StepLineDTO> stepLinesDTO) {
+        List<StepLine> listStepLines = new ArrayList<>();
+        for (StepLineDTO stepLineDTO : stepLinesDTO) {
+            listStepLines.add(toEntity(stepLineDTO));
+        }
+        return listStepLines;
+    }
+}

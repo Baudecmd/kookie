10425866420cdb8bfceabf9db8d1fef16dcde442
Jonelle Amio <jonelle.amio@gmail.com>
2021-03-30 17:37:39 +0000

    feat : authentication ui

---
+++ mobile/kookie/lib/controllers/text_form_field.dart
@@ -0,0 +1,39 @@
+/// Check if a String is a valid email.
+/// Return null if it is valid.
+String? isEmail(String val) {
+  // Null or empty string is invalid
+  if (val == null || val.isEmpty) {
+    return 'Veuillez saisir votre email';
+  }
+
+  const pattern = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+  final regExp = RegExp(pattern);
+
+  if (!regExp.hasMatch(val)) {
+    return 'Email non valide';
+  }
+  return null;
+}
+
+/// Check if a String is a valid password.
+/// Return null if it is valid.
+/// Minimum 1 Upper case
+/// Minimum 1 lowercase
+/// Minimum 1 Numeric Number
+/// Minimum 1 Special Character
+/// Common Allow Character ( ! @ # $ & * ~ )
+String? isGoodPwd(String val) {
+  // Null or empty string is invalid
+  if (val == null || val.isEmpty) {
+    return 'Veuillez saisir votre mot de passe';
+  }
+
+  const pattern =
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+  final regExp = RegExp(pattern);
+
+  if (!regExp.hasMatch(val)) {
+    return 'Utilisez un mot de passe fort';
+  }
+  return null;
+}

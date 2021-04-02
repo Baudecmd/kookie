0e32ffcf3bba7ed07d0ece235e6b33fc32ba61bd
Audran Landuyt <audran.landuyt.etu@univ-lille.fr>
2021-04-02 15:04:25 +0200

    debug mode banner false

--- mobile/kookie/lib/main.dart
+++ mobile/kookie/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Kookie',
       theme: ThemeData(
         primaryColor: Color(0xFF66C586),

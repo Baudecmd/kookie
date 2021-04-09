02d9810d5573bdef0735e811a347c52e9de4a377
Audran Landuyt <audran.landuyt.etu@univ-lille.fr>
2021-04-09 14:51:09 +0200

    Create profile service, profile controller & repository

---
+++ api/src/main/java/com/api/kookie/core/profile/ProfileServiceImpl.java
@@ -0,0 +1,39 @@
+package com.api.kookie.core.profile;
+
+import com.api.kookie.core.dto.ProfileDTO;
+import com.api.kookie.core.user.UserService;
+import com.api.kookie.core.util.ProfileParser;
+import com.api.kookie.data.profile.ProfileRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class ProfileServiceImpl implements ProfileService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProfileService.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Override
+    @Transactional
+    public ProfileDTO createProfile(ProfileDTO profile) {
+
+        LOGGER.debug("[ProfileService, createProfile] profile = " + profile.toString());
+
+        ProfileDTO profileDTO = new ProfileDTO();
+        Boolean usernameAlreadyExists = userService.usernameAlreadyExists(profile.getUser().getUsername());
+        Boolean emailAlreadyExists = userService.emailAlreadyExists(profile.getUser().getEmail());
+        if (!usernameAlreadyExists && !emailAlreadyExists) {
+            profileDTO = ProfileParser.toDTO(profileRepository.save(ProfileParser.toEntity(profile)));
+        }
+        return profileDTO;
+    }
+}

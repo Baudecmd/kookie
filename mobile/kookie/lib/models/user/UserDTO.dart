8cd906e976bd6040466258603b48fdaae6f782df
Audran Landuyt <audran.landuyt.etu@univ-lille.fr>
2021-04-09 17:28:14 +0200

    Add password in UserDTO.dart

--- mobile/kookie/lib/models/user/UserDTO.dart
+++ mobile/kookie/lib/models/user/UserDTO.dart
@@ -7,9 +7,10 @@ class UserDTO {
   final int? id;
   final String? email;
   final String? username;
+  final String? password;
   final String? token;
 
-  UserDTO({this.id, this.email, this.username, this.token});
+  UserDTO({this.id, this.email, this.username, this.password, this.token});
 
   factory UserDTO.fromJson(Map<String, dynamic> json) =>
       _$UserDTOFromJson(json);

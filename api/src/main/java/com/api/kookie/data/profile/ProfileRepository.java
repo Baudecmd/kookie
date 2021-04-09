416492f9d106f2f6f73416a7495e9f97b4ab337c
Audran Landuyt <audran.landuyt.etu@univ-lille.fr>
2021-04-09 14:51:04 +0200

    Create profile service, profile controller & repository

---
+++ api/src/main/java/com/api/kookie/data/profile/ProfileRepository.java
@@ -0,0 +1,9 @@
+package com.api.kookie.data.profile;
+
+import com.api.kookie.data.entity.Profile;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProfileRepository extends CrudRepository<Profile, Integer> {
+
+    Profile findOneByUser_Username(String username);
+}

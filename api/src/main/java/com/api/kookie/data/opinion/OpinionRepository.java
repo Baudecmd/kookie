935055a33b2aef5f4bdd9a92ffc24bc976eb1960
Audran Landuyt <audran.landuyt.etu@univ-lille.fr>
2021-05-04 22:52:34 +0200

    Change import

--- api/src/main/java/com/api/kookie/data/opinion/OpinionRepository.java
+++ api/src/main/java/com/api/kookie/data/opinion/OpinionRepository.java
@@ -1,6 +1,6 @@
 package com.api.kookie.data.opinion;
 
-import com.api.kookie.data.entity.Ingredient;
+import com.api.kookie.data.entity.ingredient.Ingredient;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OpinionRepository extends CrudRepository<Ingredient, Integer> {

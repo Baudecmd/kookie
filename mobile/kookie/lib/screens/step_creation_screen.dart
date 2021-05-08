913afd69ff88f78a0f88e408d731c723fd3c590c
Lilian Delouvy <lilian.delouvy@gmail.com>
2021-05-08 16:46:19 +0200

    Feat(Step Creation): page layout

--- mobile/kookie/lib/screens/step_creation_screen.dart
+++ mobile/kookie/lib/screens/step_creation_screen.dart
@@ -1,34 +1,106 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kookie/widgets/custom_button.dart';
+
+class MultiSelectItem {
+  const MultiSelectItem(this.value, this.label);
+
+  final int value;
+  final String label;
+}
 
 class StepCreationScreen extends StatefulWidget {
+  late final List<MultiSelectItem> _utensilsList;
+
+  StepCreationScreen() {
+    _utensilsList = _getUtensilsList();
+  }
+
   @override
   _StepCreationScreen createState() => _StepCreationScreen();
+
+  _getUtensilsList() {
+    return [
+      MultiSelectItem(1, "Couteaux de cuisine"),
+      MultiSelectItem(2, "Four"),
+      MultiSelectItem(3, "Épluche-légumes")
+    ];
+  }
 }
 
 class _StepCreationScreen extends State<StepCreationScreen> {
+  final textKey = GlobalKey<FormState>();
+  final utensilsListKey = GlobalKey<FormState>();
+  final _selectedUtensils = Set<int>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            image: DecorationImage(
+              image: AssetImage('assets/images/login_page_bg.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
           child: Column(
             children: [
-              SizedBox(height: 30),
+              SizedBox(height: 50),
               Center(
                 child: Text("Décrivez cette étape !"),
               ),
-              TextField(
-                key: GlobalKey<FormState>(),
-                keyboardType: TextInputType.multiline,
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: TextField(
+                  key: textKey,
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                ),
               ),
-              SizedBox(height: 10),
-              Center(child: Text("Quel est le matériel requis ?")),
-              SizedBox(height: 10)
+              SizedBox(height: 20),
+              Center(
+                child:
+                    Text("Quels sont les ustensiles requis pour cette étape ?"),
+              ),
+              SizedBox(height: 20),
+              ListBody(
+                children: widget._utensilsList.map(_buildItem).toList(),
+              ),
+              Spacer(),
+              CustomButton(
+                text: "Valider",
+                onTap: _submitStepData,
+              ),
+              SizedBox(height: 20),
             ],
           ),
         ),
       ),
     );
   }
+
+  Widget _buildItem(MultiSelectItem item) {
+    final checked = _selectedUtensils.contains(item.value);
+    return CheckboxListTile(
+      value: checked,
+      title: Text(item.label),
+      controlAffinity: ListTileControlAffinity.leading,
+      onChanged: (checked) => _onUtensilCheckedChange(item.value, checked!),
+    );
+  }
+
+  _onUtensilCheckedChange(int value, bool checked) {
+    setState(() {
+      if (checked) {
+        _selectedUtensils.add(value);
+      } else {
+        _selectedUtensils.remove(value);
+      }
+    });
+  }
+
+  _submitStepData() {}
 }

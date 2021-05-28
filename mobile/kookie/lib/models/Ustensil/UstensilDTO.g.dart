ff15afad7d829fcc878b2f111e68d08ca2f4e12b
Audran Landuyt <audran.landuyt.etu@univ-lille.fr>
2021-05-28 15:38:12 +0200

    feat(KOOKIE): Update DTO from Spring DTOs

---
+++ mobile/kookie/lib/models/Ustensil/UstensilDTO.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'UstensilDTO.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UstensilDTO _$UstensilDTOFromJson(Map<String, dynamic> json) {
+  return UstensilDTO(
+    id: json['id'] as int?,
+    name: json['name'] as String?,
+  );
+}
+
+Map<String, dynamic> _$UstensilDTOToJson(UstensilDTO instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };

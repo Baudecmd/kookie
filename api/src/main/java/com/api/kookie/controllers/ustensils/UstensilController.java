74e195c400b5df1d3b3e4937a9776fab4a149498
Audran Landuyt <audran.landuyt.etu@univ-lille.fr>
2021-06-04 20:30:09 +0200

    fix(kookie): Replace headers by produces and content-type by charset

--- api/src/main/java/com/api/kookie/controllers/ustensils/UstensilController.java
+++ api/src/main/java/com/api/kookie/controllers/ustensils/UstensilController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(produces = "application/json; charset=utf-8")
+@RequestMapping(produces = "application/json;charset=utf-8")
 public class UstensilController {
 
 

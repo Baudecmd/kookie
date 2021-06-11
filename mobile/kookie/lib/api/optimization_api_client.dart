7844999293c9feffe70288602755a1b37d3ef9f8
Lilian Delouvy <lilian.delouvy@gmail.com>
2021-06-11 14:02:38 +0200

    refactor(Optimization): encore list content

--- mobile/kookie/lib/api/optimization_api_client.dart
+++ mobile/kookie/lib/api/optimization_api_client.dart
@@ -7,8 +7,7 @@ import 'package:kookie/models/step/StepDTO.dart';
 class OptimizationApiClient extends LoginApiClient {
   Future<StepDTO> optimizeSession(Set<int> recipesIds) async {
     Map<String, dynamic> map = Map<String, dynamic>();
-    map["RecipesIds"] = recipesIds.toList();
-    jsonEncode(map);
+    map["RecipesIds"] = jsonEncode(recipesIds.toList());
     final http.Response response =
         await postData('/recipesOptimization/optimize_recipes_list', map);
     if (response.statusCode == 200) {

310e4ed4ee8b76b89e56f5810676e98f3acf1847
Lilian Delouvy <lilian.delouvy@gmail.com>
2021-06-11 16:55:07 +0200

    fix(Optimization): set RecipesOptimizationController as Controller

--- api/src/main/java/com/api/kookie/controllers/recette/RecipesOptimizationController.java
+++ api/src/main/java/com/api/kookie/controllers/recette/RecipesOptimizationController.java
@@ -9,10 +9,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping(value = "/recipesOptimization", produces = "application/json; charset=utf-8")
 public class RecipesOptimizationController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RecetteController.class);

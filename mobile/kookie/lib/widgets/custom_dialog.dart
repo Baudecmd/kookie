9040fd63e61aa61bf37f3c701276174a7b1e1a44
Jonelle Amio <jonelle.amio@gmail.com>
2021-06-18 21:27:36 +0000

    Fix/jonelle

---
+++ mobile/kookie/lib/widgets/custom_dialog.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomDialog extends StatelessWidget {
+  const CustomDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: ExactAssetImage('assets/images/chargement.gif'),
+                fit: BoxFit.cover)),
+      ),
+    );
+  }
+}
